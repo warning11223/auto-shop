a37fde080fff220ba22f01d41d2a3ce712006173
warning11223 <warning11223@gmail.com>
2025-06-16 13:45:28 +0300

    refactor: скелетоны

--- components/CardSlider/index.tsx
+++ components/CardSlider/index.tsx
@@ -6,13 +6,16 @@ import 'swiper/css/pagination';
 import Image from 'next/image';
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
-import {useMemo} from "react";
+import { useMemo, useState } from "react";
+import Skeleton from "react-loading-skeleton";
 
 type Props = {
-    images: string[]
+    images: string[];
 }
 
 export default function CarSlider({ images }: Props) {
+    const [loading, setLoading] = useState(true);
+
     const limitedImages = useMemo(() => images.slice(0, 12), [images]);
 
     return (
@@ -26,14 +29,22 @@ export default function CarSlider({ images }: Props) {
                 modules={[Navigation, Pagination]}
             >
                 {limitedImages.map((img, idx) => (
-                    <SwiperSlide key={idx} className="xl:min-h-[250px]">
-                        <Image
-                            src={img}
-                            alt={`Car image ${idx}`}
-                            className="w-full h-full object-cover rounded-lg cursor-pointer"
-                            width={500}
-                            height={300}
-                        />
+                    <SwiperSlide key={idx}>
+                        <div className="relative w-full h-[150px] md:h-[250px]">
+                            {loading && (
+                                <Skeleton height="100%" width="100%"/>
+                            )}
+                            <Image
+                                id={`img-${idx}`}
+                                src={img}
+                                alt={`Car image ${idx}`}
+                                className={`w-full h-full object-cover rounded-lg cursor-pointer transition-opacity duration-300`}
+                                layout="fill"
+                                onLoad={() => setLoading(false)}
+                                style={{opacity: loading ? '0' : '1'}}
+                            />
+                        </div>
+
                     </SwiperSlide>
                 ))}
             </Swiper>
--- components/Skeletons/index.tsx
+++ components/Skeletons/index.tsx
@@ -3,11 +3,11 @@ import Skeleton from "react-loading-skeleton";
 export const Skeletons = () => {
     return (
         [...Array(12)].map((_, index) => (
-            <div key={index} className="relative border border-gray-300 rounded-lg p-6 shadow-lg">
+            <div key={index} className="relative border border-gray-300 rounded-lg p-4 sm:p-6 shadow-lg">
                 <Skeleton height={32} width="100%" className="mb-4" />
-                <Skeleton height={240} width="100%" />
+                <Skeleton width="100%" className="h-[140px] md:h-[240px]" />
                 <Skeleton height={28} width="70%" className="mt-4" />
-                <Skeleton height={28} width="40%" />
+                <Skeleton height={28} width="40% mt-4" className="mt-2" />
             </div>
         ))
     );
